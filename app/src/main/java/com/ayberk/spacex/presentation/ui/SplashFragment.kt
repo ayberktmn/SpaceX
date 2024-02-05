e303e6961acb63e0192eef8bc853fdfdfc814a49
ayberktmn <ayberktemin@gmail.com>
2024-02-05 15:52:58 +0300

    first commit

---
+++ app/src/main/java/com/ayberk/spacex/presentation/ui/SplashFragment.kt
@@ -0,0 +1,38 @@
+package com.ayberk.spacex.presentation.ui
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.ayberk.spacex.R
+import com.ayberk.spacex.databinding.FragmentSplashBinding
+
+
+class SplashFragment : Fragment() {
+
+    private var _binding : FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = FragmentSplashBinding.inflate(inflater,container,false)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_mainFragment)
+        },3000)
+
+        return binding.root
+
+    }
+}
